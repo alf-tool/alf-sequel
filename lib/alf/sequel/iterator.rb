8cb9b793049bb4a12c8bdfd56f1ee6adcc4091d7
Bernard Lambeau <blambeau@gmail.com>
2012-07-28 16:34:49 +0200

    Add Iterator#sql

--- lib/alf/sequel/iterator.rb
+++ lib/alf/sequel/iterator.rb
@@ -10,10 +10,15 @@ module Alf
       def initialize(dataset)
         @dataset = dataset
       end
+      attr_reader :dataset
 
       # (see Alf::Iterator#each)
       def each
         @dataset.each(&Proc.new)
+      end
+
+      def sql
+        dataset.sql
       end
 
     end # class Iterator
