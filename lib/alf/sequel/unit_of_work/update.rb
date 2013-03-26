7a2458ca604c8268ca08084f9b14106663a3e139
Bernard Lambeau <blambeau@gmail.com>
2013-03-26 23:05:46 +0100

    Add matching_relation and pk_matching_relation to UnitOfWork::Update

--- lib/alf/sequel/unit_of_work/update.rb
+++ lib/alf/sequel/unit_of_work/update.rb
@@ -6,9 +6,44 @@ module Alf
 
         def initialize(connection, relvar_name, updating, predicate)
           super(connection)
-          @relvar_name   = relvar_name
-          @updating      = updating
-          @predicate     = predicate
+          @relvar_name = relvar_name
+          @updating    = updating
+          @predicate   = predicate
+        end
+
+        def matching_relation
+          raise IllegalStateError, "Unit of work not ran" unless ran?
+
+          # Check that updating is understandable
+          unless TupleLike===@updating
+            raise UnsupportedError, "Non-tuple update feedback is unsupported."
+          end
+          updating = @updating.to_hash
+          updating_keys = updating.keys
+
+          # extract all keys, and pk in particular
+          keys = connection.keys(@relvar_name)
+          pk   = keys.first
+
+          # Strategy 1), @updating contains a key
+          if key = keys.to_a.find{|k| !(k & updating_keys).empty? }
+            return Relation(@updating.project(key).to_hash)
+          end
+
+          raise UnsupportedError, "Unable to extract update matching relation"
+        end
+
+        def pk_matching_relation
+          mr, pkey = matching_relation, connection.keys(@relvar_name).first
+          if mr.to_attr_list == pkey.to_attr_list
+            mr
+          else
+            filter = mr.tuple_extract.to_hash
+            tuples = connection.cog(@relvar_name)
+                               .filter(filter)
+                               .select(pkey.to_a)
+            Relation(tuples)
+          end
         end
 
       private
