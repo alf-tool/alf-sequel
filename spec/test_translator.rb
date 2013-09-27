07aa36f41c6227493f0621d56bd19a9653a176d2
Bernard Lambeau <blambeau@gmail.com>
2013-09-27 12:34:08 +0200

    Don't try to compile non-sqlizable tests.

--- spec/test_translator.rb
+++ spec/test_translator.rb
@@ -6,6 +6,8 @@ module Alf
       subject{ Translator.new(db).call(sql_cog.sexpr) }
 
       Alf::Test.each_query do |query|
+        next unless query['sql']
+
         context "on `#{query['query']}`" do
           let(:alf_ast){ conn.parse(query['query']) }
           let(:sql_cog){ Alf::Sql::Compiler.new.call(alf_ast) }
