728b73240755e9724bc4915a338220ab29eb5298
Bernard Lambeau <blambeau@gmail.com>
2013-10-01 09:53:14 +0200

    Follow alf-core changes about compilation plan.

--- lib/alf/sequel/compiler.rb
+++ lib/alf/sequel/compiler.rb
@@ -14,12 +14,12 @@ module Alf
         Cog.new(expr, self, sexpr, connection)
       end
 
-      def rewrite(compiled, expr, processor, args = [])
+      def rewrite(plan, expr, compiled, processor, args = [])
         rewrited = processor.new(*args.push(builder)).call(compiled.sexpr)
         Cog.new(expr, self, rewrited, compiled.connection)
       end
 
-      def rebind(compiled, expr)
+      def rebind(plan, expr, compiled)
         Cog.new(expr, self, compiled.sexpr, compiled.connection)
       end
 
